32a1c47334860a299e0a2ea9023e116e63a463bb
thomasaiman <32423251+thomasaiman@users.noreply.github.com>
2023-02-24 21:40:19 -0500

    improved gather implementation

--- spider-attack/submission.py
+++ spider-attack/submission.py
@@ -1,8 +1,9 @@
 import sys
 import math
-from typing import List, Union, Dict
+from typing import List, Union, Dict, Iterator, Tuple, Optional
 from enum import Enum
-from collections import namedtuple
+from collections import namedtuple, OrderedDict
+from itertools import chain
 
 Numeric = Union[int, float]
 
@@ -18,6 +19,9 @@ class Point:
 
     def __str__(self):
         return f"Point({self.x} {self.y})"
+
+    def __hash__(self):
+        return hash((self.x, self.y))
 
     def dist(self, other: 'Point'):
         return math.sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
@@ -60,6 +64,25 @@ class Vector:
 
     def __str__(self):
         return f"Vector({self.x} {self.y})"
+
+
+def circle_intersection(p1:Point, p2:Point, r: Numeric) -> Tuple[Point, Point]:
+    """Calculate the intersection points of two circles with the same radius."""
+    x1,y1,x2,y2 = p1.x, p1.y, p2.x, p2.y
+    d = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)  # distance between circle centers
+    if d > 2 * r:  # circles do not overlap
+        raise ValueError('no overlap')
+    else:
+        a = (r**2 - (d/2)**2) ** 0.5
+        xm = (x1 + x2) / 2
+        ym = (y1 + y2) / 2
+        xs1 = xm + a * (y2 - y1) / d
+        xs2 = xm - a * (y2 - y1) / d
+        ys1 = ym - a * (x2 - x1) / d
+        ys2 = ym + a * (x2 - x1) / d
+        return (Point(xs1, ys1), Point(xs2, ys2))
+
+
 
 class EntityType(Enum):
     MONSTER = 0
@@ -118,7 +141,10 @@ class EntityTracker:
 
     def update(self, n: int):
         [m.advance() for m in self.monsters.values()]
+        for e in self.all_entities():
+            e.is_winded = False
         self.prune_monsters()
+        self.opps = {}
         updates: List[EntityBase] = [entityBaseFactory(input()) for _ in range(n)]
         for eb in updates:
             if eb.etype == EntityType.MONSTER:
@@ -144,6 +170,8 @@ class EntityTracker:
         
         [self.monsters.pop(id) for id in to_remove]
 
+    def all_entities(self) -> Iterator['Entity']:
+        return chain(self.heroes.values(), self.opps.values(), self.monsters.values())
 class Entity:
     # _id: Unique identifier
     # _type: 0=monster, 1=your hero, 2=opponent hero
@@ -164,6 +192,7 @@ class Entity:
         self.vel = eb.vel
         self.near_base = eb.near_base
         self.threat_for = eb.threat_for
+        self.is_winded = False
 
     def advance(self):
         raise NotImplementedError()
@@ -207,6 +236,8 @@ class Monster(Entity):
         else:
             return False
 
+    def is_handled(self) -> bool:
+        raise NotImplementedError()
 class Hero(Entity):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -215,6 +246,9 @@ class Hero(Entity):
 
     def _wind(self, point:Point):
         self.command = f"{CommandType.WIND.value} {int(point.x)} {int(point.y)}"
+        for e in ENTITIES.all_entities():
+            if self.dist(e) < WIND_RANGE:
+                e.is_winded = True
 
     def _move(self, point:Point):
         self.command = f"{CommandType.MOVE.value} {int(point.x)} {int(point.y)}"
@@ -234,10 +268,13 @@ class Hero(Entity):
     def defensive_wind(self, t: Entity):
         v: Vector = BASE.vector_to(t.loc)
         self._wind(self.loc.add_vector(v))
+        t.is_winded = True
+        debug(f"wind {t.id}")
 
     def offensive_wind(self, t: Entity):
         v: Vector = t.loc.vector_to(OPP_BASE)
         self._wind(self.loc.add_vector(v))
+        debug(f"wind {t.id}")
 
     def attack_monster(self, t: Entity):
         v: Vector = t.loc.vector_to(self.loc)
@@ -255,6 +292,38 @@ class Hero(Entity):
 
     def opp_neighbors(self) -> List['OppHero']:
         return [o for o in opps if o.dist(self.loc)<CONTROL_RANGE]
+
+    def optimal_attack(self) -> Optional[Point]:
+        SEARCH_RANGE = HERO_SPEED + MELEE_RANGE - 10 # for rounding errors
+        neighbors: List[Monster] = [m for m in ENTITIES.monsters.values() if m.dist(self) < SEARCH_RANGE]
+        if not neighbors:
+            return None
+        debug(f" hero {self.id} sees monsters {[m.id for m in neighbors]}")
+        candidates: Dict[Point, List[Monster]] = {}
+        for m in neighbors:
+            p: Point = Point.centroid([self.loc, m.loc])
+            candidates[p] = []
+        for i,m1 in enumerate(neighbors):
+            for m2 in neighbors[i+1:]:
+                if m1.dist(m2) > SEARCH_RANGE:
+                    continue
+                else:
+                    p1,p2 = circle_intersection(m1.loc, m2.loc, SEARCH_RANGE/2)
+                    if self.dist(p1) < SEARCH_RANGE/2:
+                        candidates[p1] = [m1,m2]
+                    if self.dist(p2) < SEARCH_RANGE/2:
+                        candidates[p2] = [m1, m2]
+
+        for p in candidates.keys():
+            for m in neighbors:
+                if m.dist(p) < SEARCH_RANGE/2:
+                    candidates[p].append(m)
+        
+        result: Point = max(candidates.keys(), key=lambda k:len(candidates[k]))
+        debug(f"{result} attacks monsters {[m.id for m in candidates[result]]}")
+        return result
+
+        
 
 
 class AtkHero(Hero):
@@ -276,7 +345,7 @@ class AtkHero(Hero):
 
     def instakill(self) -> bool:
         for m in ENTITIES.monsters.values():
-            if (m.dist(OPP_BASE) < WIND_MOVE+400) and (self.dist(m) < WIND_RANGE):
+            if (m.dist(OPP_BASE) < WIND_MOVE+400) and (self.dist(m) < WIND_RANGE) and m.shield_life==0:
                 self.offensive_wind(m)
                 return True
         else:
@@ -307,11 +376,16 @@ class AtkHero(Hero):
 
 
     def gather(self):
-        if monsters:
+        p: Optional[Point] = self.optimal_attack()
+        if p and p.dist(BASE)>BASE_VISION:
+            self._move(p)
+        elif monsters:
             target: Monster = min(monsters, key=lambda m: m.loc.dist(HERO3_HOME))
             self.attack_monster(target)
-            return
-        self._move(self.home)
+        else:
+            self._move(self.home)
+        
+
 
 
 class MidHero(Hero):
@@ -332,6 +406,7 @@ class MidHero(Hero):
         if self.shield_life>0 and MANA_ME > 40 and on:
             if on[0].shield_life == 0 and on[0].loc.dist(self.loc) < WIND_RANGE:
                 t = on[0]
+                debug(f"opploc={on[0].loc} dist={on[0].loc.dist(self.loc)}")
                 v: Vector = t.loc.vector_to(JAIL)
                 self.defensive_wind(t)
                 return
@@ -348,8 +423,12 @@ class MidHero(Hero):
             return
 
         if t.etype == EntityType.MONSTER:
-            if t.dist(BASE) < (MONSTER_VISION-WIND_MOVE) and self.loc.dist(t.loc) < WIND_RANGE and t.shield_life==0 and MANA_ME>10:
-                self.defensive_wind(t)
+            if t.dist(BASE) < (MONSTER_VISION-WIND_MOVE) \
+                and self.loc.dist(t.loc) < WIND_RANGE \
+                and t.shield_life==0 \
+                and MANA_ME>10 \
+                and t.is_winded == False:
+                    self.defensive_wind(t)
             # elif MANA_ME > 100 and self.loc.dist(t.loc) < WIND_RANGE and t.shield_life==0:
             #     self.offensive_wind(t)
             else:
@@ -381,13 +460,16 @@ class DefHero(Hero):
             self._move(self.home)
             return
 
-        if t.etype == EntityType.MONSTER:
-            if t.dist(BASE) < (MONSTER_VISION-WIND_MOVE) and self.loc.dist(t.loc) < WIND_RANGE and t.shield_life==0 and MANA_ME>10:
+        if t.dist(BASE) < (MONSTER_VISION-WIND_MOVE) \
+            and self.loc.dist(t.loc) < WIND_RANGE \
+            and t.shield_life==0 \
+            and MANA_ME>10 \
+            and t.is_winded == False:
                 self.defensive_wind(t)
             # elif MANA_ME > 100 and self.loc.dist(t.loc) < WIND_RANGE and t.shield_life==0:
             #     self.offensive_wind(t)
-            else:
-                self.attack_monster(t)
+        else:
+            self.attack_monster(t)
 
 
 class OppHero(Hero):
